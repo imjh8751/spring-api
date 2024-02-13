534a9048f941e52b362c6a0fd5f1a94a2adb6bd5
imjh8751 <imjh8751@daum.net>
2024-02-13 14:13:28 +0000

    redis commit

--- src/main/java/com/betwe/eurekaserver/controller/RedisController.java
+++ src/main/java/com/betwe/eurekaserver/controller/RedisController.java
@@ -1,8 +1,64 @@
 package com.betwe.eurekaserver.controller;
 
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/redis")
 public class RedisController {
+
+    private static final String CLIENT_INFO_KEY = "client_info";
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @GetMapping("/save")
+    public void saveClientInfo(jakarta.servlet.http.HttpServletRequest request) {
+        String clientIp = request.getRemoteAddr();
+        String jsession = request.getSession().getId();
+        String country = clientIp;
+        long currentTime = Instant.now().toEpochMilli();
+
+    	System.out.println("saveClientInfo ::: " + clientIp + " / jsession ::: " + jsession);
+    	
+        String key = "client:" + clientIp;
+        Map<String, Object> data = new HashMap<>();
+        data.put("clientIp", clientIp);
+        data.put("jsession", jsession);
+        data.put("firstAccessTime", currentTime);
+        data.put("lastAccessTime", currentTime);
+        data.put("country", country);
+
+        redisTemplate.opsForHash().putAll(key, data);
+        redisTemplate.expire(key, 600, TimeUnit.SECONDS); // 10 minutes TTL
+    }
+
+    @GetMapping("/get")
+    public Map<String, Object> getClientInfo(String clientIp, String jSession) {
+        System.out.println("test get ::: ");
+        String key = getClientKey(clientIp, jSession);
+        HashOperations<String, String, Object> hashOperations = redisTemplate.opsForHash();
+        return hashOperations.entries(key);
+    }
+
+    private String getClientKey(String clientIp, String jSession) {
+        return CLIENT_INFO_KEY + ":" + clientIp + ":" + jSession;
+    }
+    
+	@GetMapping("/getSessionId")
+	public String getSessionId(HttpSession session) {
+		return session.getId();
+	}
     
 }
--- src/main/java/com/betwe/eurekaserver/util/RedisConfiguration.java
+++ src/main/java/com/betwe/eurekaserver/util/RedisConfiguration.java
@@ -9,30 +9,29 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfiguration {
-	@Value("${spring.data.redis.host}")
-	private String redisHost;
-	@Value("${spring.data.redis.port}")
-	private int redisPort;
-	@Value("${spring.data.redis.password}")
-	private String redisPassword;
+	//@Value("${spring.data.redis.host}")
+	//private String redisHost;
+	//@Value("${spring.data.redis.port}")
+	//private int redisPort;
+	//@Value("${spring.data.redis.password}")
+	//private String redisPassword;
 	@Value("${spring.data.redis.cluster.nodes}")
 	private String clusterNodes;
 	
 	@Bean
-	public RedisConnectionFactory redisConnectionFactory() {
+	public LettuceConnectionFactory lettuceConnectionFactory() {
 		List<String> clusterNodeList = Arrays.asList(StringUtils.split(clusterNodes, ','))
                 .stream()
                 .map(m -> m.trim())
                 .collect(Collectors.toList());
 		RedisClusterConfiguration redisStandaloneConfiguration = new RedisClusterConfiguration(clusterNodeList);
-		redisStandaloneConfiguration.setPassword(redisPassword);
+		//redisStandaloneConfiguration.setPassword(redisPassword);
 		LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
 		return lettuceConnectionFactory;
 	}
@@ -40,7 +39,7 @@ public class RedisConfiguration {
 	@Bean
 	public RedisTemplate<String, Object> redisTemplate() {
 		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		redisTemplate.setConnectionFactory(lettuceConnectionFactory());
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
 		redisTemplate.setValueSerializer(new StringRedisSerializer());
 		return redisTemplate;
