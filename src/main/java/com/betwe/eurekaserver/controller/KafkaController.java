b5d7049303360a61f8b67aed0ef7c0efe03c0675
imjh8751 <imjh8751@daum.net>
2024-02-21 14:22:44 +0000

    commit

--- src/main/java/com/betwe/eurekaserver/controller/KafkaController.java
+++ src/main/java/com/betwe/eurekaserver/controller/KafkaController.java
@@ -1,11 +1,12 @@
 package com.betwe.eurekaserver.controller;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,13 @@ public class KafkaController {
         this.producer = producer;
     }
 
+    @GetMapping("/")
+    public String getMessage() {
+        // 디버그 로그 출력
+        log.info("Debug log message");
+        return "good request";
+    }
+
     @PostMapping(value = "/send")
     public String sendMessage(@RequestParam("message") String message) {
         log.debug("message : " + message);
@@ -31,10 +39,24 @@ public class KafkaController {
         return "success";
     }
 
-    @GetMapping(value = "/send")
-    public String sendMessage2(@RequestParam("message") String message) {
+    @GetMapping(value = "/send2/{message}")
+    public String sendMessage2(@PathVariable String message) {
         log.debug("message : " + message);
         this.producer.sendMessage("test", message);
+        return "success";
+    }
+    
+    @GetMapping("/send3")
+    public String sendMessage3(@RequestBody String message) {
+    	log.debug("message topic ::: " + message);
+        this.producer.sendMessage("test", message);
+        return "success";
+    }
+    
+    @GetMapping("/send4")
+    public String sendMessage3() {
+    	log.debug("message topic ::: testteset");
+        this.producer.sendMessage("test", "testteset!!!!!!!!!");
         return "success";
     }
 }
--- src/main/java/com/betwe/eurekaserver/service/KafkaProducerService.java
+++ src/main/java/com/betwe/eurekaserver/service/KafkaProducerService.java
@@ -1,6 +1,5 @@
 package com.betwe.eurekaserver.service;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
